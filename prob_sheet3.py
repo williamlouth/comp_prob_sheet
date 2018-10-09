e45e2019ba1b25636fabd4a3ec906b1080483074
william <williamlouth@outlook.com>
2018-10-09 18:56:19 +0100

    modified:   prob_sheet3.py

--- prob_sheet3.py
+++ prob_sheet3.py
@@ -23,21 +23,31 @@ def M_multiply(a,b):
     
 
 def Gaussian(A,b):
+    print(A)
+    
+    
+    A00 = A[0][0]
     for i in range(A.shape[1]):
-        print("hiu")    
-        print(A)
-        
-        
-        A[1][i] = A[1][i]*A[0][0]/A[1][i] - A[0][i]
-        b[1] = b[1]*A[0][0]/A[1][i] - b[0]
-        
-        A[2][i] = A[2][i]*A[0][0]/A[2][i] - A[0][i]
-        b[2] = b[2]*A[0][0]/A[2][i] - b[0]
-        
-        print("here")
-        A[0][i] = A[0][i]/A[0][0]
-        b[0] = b[0]/A[0][0]
+        A[0][i] = A[0][i]/A00
+    b[0] = b[0]/A00
 
+    
+    A10 = A[1][0]    
+    for i in range(A.shape[1]):
+        A[1][i] = A[1][i]*A[0][0]/A10 - A[0][i]
+    b[1] = b[1]*A[0][0]/A10 - b[0]
+        
+    A20 = A[2][0]    
+    for i in range(A.shape[1]):
+        A[2][i] = A[2][i]*A[0][0]/A20 - A[0][i]
+    b[2] = b[2]*A[0][0]/A20 - b[0]
+
+    
+    A11 = A[1][1]    
+    A21 = A[2][1]
+    for i in range(A.shape[1]):
+        A[2][i] = A[2][i]*A11/A21 - A[1][i]
+    b[2] = b[2]*A11/A21 - b[1]
         #print(b[])
     
     return A,b
@@ -51,8 +61,8 @@ print(np.dot(a,b))
 
 
 
-my_A = np.array([[5,6,77],[7,8,12],[1,2,65]])
-my_b = np.array([[2],[5],[24]])
+my_A = np.array([[5.,6.,77.],[7.,8.,12.],[1.,2.,65.]])
+my_b = np.array([[2.],[5.],[240]])
 print(Gaussian(my_A,my_b))
 
 
